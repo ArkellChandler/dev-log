982899a44b9481443cc754090dfcd5c6a6f28ed3
Arkell_Chandler <arkellchandler@gmail.com>
2026-03-10 02:58:46 -0300

    Exercício: implementado verificador de palíndromos

---
+++ bootcamp-TOTVS/template.py
@@ -0,0 +1,13 @@
+"""
+OBJETIVO: [Descreva o desafio aqui]
+DATA: 2026-03-10
+AUTOR: Desenvolvedor dev-log
+REF: ../assets/referencias.txt
+"""
+
+def main():
+    # Seu código aqui
+    print("Iniciando exercício...")
+
+if __name__ == "__main__":
+    main()
